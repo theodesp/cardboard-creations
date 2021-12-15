0b25ca11850ee750805cf9fc827a0c4d6d773aac
theodesp <theo.despoudis@wpengine.com>
2021-12-15 12:35:54 +0000

    Provides a fix for jest module resolution
    Signed-off-by: theodesp <theo.despoudis@wpengine.com>

--- src/components/__tests__/Header.test.tsx
+++ src/components/__tests__/Header.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 
 import Header from "../Header";
-import { client } from "client";
+import { client } from "../../client";
 import { mockMenuItems } from "components/common/mock_data";
 
 describe("Rendering the Header", () => {
